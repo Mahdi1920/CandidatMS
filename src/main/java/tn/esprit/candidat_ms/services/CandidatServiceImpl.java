620205674d4668f6710f75330d9f7b8d8ec66645
mahdi <mahdichabbouh98@gmail.com>
2025-02-16 19:01:06 +0100

    add getAll getById done update delete to fix mysql+mongo with mapper

--- src/main/java/tn/esprit/candidat_ms/services/CandidatServiceImpl.java
+++ src/main/java/tn/esprit/candidat_ms/services/CandidatServiceImpl.java
@@ -3,7 +3,6 @@ package tn.esprit.candidat_ms.services;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import tn.esprit.candidat_ms.entities.Candidat;
-import tn.esprit.candidat_ms.mappers.CandidatMapper;
 import tn.starter.mysqlShared.dto.CandidatDTO;
 import tn.starter.mysqlShared.services.IGenericServiceImpl;
 
@@ -12,6 +11,4 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CandidatServiceImpl extends IGenericServiceImpl<CandidatDTO,Candidat> implements ICandidatService{
-	//private final CandidatMapper candidatMapper;  // Ensure injection
-
 }
