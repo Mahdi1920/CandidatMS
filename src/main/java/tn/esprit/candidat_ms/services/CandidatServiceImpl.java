1fe809bea78db47fede096418a674ac26c0fec8c
mahdi <mahdichabbouh98@gmail.com>
2025-02-18 16:27:52 +0100

    discovery server eureka clients

--- src/main/java/tn/esprit/candidat_ms/services/CandidatServiceImpl.java
+++ src/main/java/tn/esprit/candidat_ms/services/CandidatServiceImpl.java
@@ -1,16 +1,12 @@
 package tn.esprit.candidat_ms.services;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import tn.esprit.candidat_ms.entities.Candidat;
 import tn.esprit.candidat_ms.feignClients.QuizClient;
 import tn.starter.mysqlShared.dto.CandidatDTO;
 import tn.starter.mysqlShared.dto.QuizDTO;
 import tn.starter.mysqlShared.services.IGenericServiceImpl;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/tn/esprit/candidat_ms/services/ICandidatService.java
+++ src/main/java/tn/esprit/candidat_ms/services/ICandidatService.java
@@ -1,6 +1,5 @@
 package tn.esprit.candidat_ms.services;
 
-import tn.esprit.candidat_ms.entities.Candidat;
 import tn.starter.mysqlShared.dto.CandidatDTO;
 import tn.starter.mysqlShared.dto.QuizDTO;
 import tn.starter.mysqlShared.services.IGenericService;
